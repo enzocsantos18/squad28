c8c85015b8bf7dcfac1dc8e2b7103558c4e6781b
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2021-03-23 22:38:45 -0300

    Alteração no Relacionamento da tabela ProductsList

--- backend/src/app/models/ProductsList.ts
+++ backend/src/app/models/ProductsList.ts
@@ -1,19 +1,17 @@
-import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  OneToMany,
-  ManyToOne,
-} from 'typeorm';
+import { Entity, Column, ManyToOne } from 'typeorm';
 import List from './List';
 import Product from './Product';
 
 @Entity('ProductsList')
 class ProductsList {
-  @ManyToOne(type => List, list => list.productsList)
+  @ManyToOne(type => List, list => list.productsList, {
+    primary: true,
+  })
   list: List;
 
-  @ManyToOne(type => Product, product => product.productsList)
+  @ManyToOne(type => Product, product => product.productsList, {
+    primary: true,
+  })
   product: Product;
 
   @Column('tinyint')
