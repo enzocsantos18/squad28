b8d29af7a1e09c823455629116cdeb4c6fb70769
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2021-03-27 20:45:11 -0300

    AuthMiddleware

---
+++ backend/src/app/middlewares/authMiddleware.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface TokenPayloadInterface {
+  id: number;
+  name: string;
+  type: string;
+  iat: number;
+  exp: number;
+}
+
+function AuthMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Response | void {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.sendStatus(401);
+  }
+
+  const token = authorization.replace('Bearer', '').trim();
+
+  try {
+    const data = jwt.verify(token, String(process.env.JWT_SECRET));
+
+    const { id, type } = data as TokenPayloadInterface;
+
+    if (type === 'parent') {
+      req.parentId = id;
+    } else {
+      req.storeId = id;
+    }
+  } catch {
+    return res.sendStatus(401);
+  }
+
+  return next();
+}
+
+export default AuthMiddleware;
