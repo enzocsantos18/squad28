82b4413df7c418795cfe0e194fc7619123b5ff0e
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2021-03-27 09:57:48 -0300

    Listas de escolas filtrradas por bairro

--- backend/src/app/controllers/SchoolController.ts
+++ backend/src/app/controllers/SchoolController.ts
@@ -4,10 +4,16 @@ import School from '../models/School';
 
 class SchoolController {
   async index(req: Request, res: Response) {
+    const { neighborhood } = req.query;
+
     const SchoolRepository = getRepository(School);
 
     try {
-      const schools = await SchoolRepository.find();
+      const schools = await SchoolRepository.find({
+        where: {
+          neighborhood,
+        },
+      });
       return res.json(schools);
     } catch (e) {
       return res.status(400).json({ error: 'Schools not found.' });
