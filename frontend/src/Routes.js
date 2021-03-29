c06ad9aafc9d832f39a70f302fac7ca10d66bde0
Carolina Andrade <cpandrade91@gmail.com>
2021-03-29 18:47:40 -0300

    Cadastro Responsável Mobile
    Cadastro Responsável em formado Mobile.
    Feito: Campos e Validações
    CSS: mobile
    Falta:
    - CSS desktop
    - Mask
    - Menu e Rodapé

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './pages/Home'
 import Login from './pages/Login';
 import DashboardStore from './pages/DashboardStore';
+import CadastroResponsavel from './pages/CadastroResponsavel';
 
 
 function Routes() {
   return (
     <Router>
       <Switch>
+        <Route path="/cadastroResponsavel" component={CadastroResponsavel} />
         <Route path="/" exact component={Home} />
         <Route path="/dashboard/store" component={DashboardStore}/>
         <Route path="/login" component={Login}/>
---
+++ frontend/src/pages/CadastroResponsavel/index.js
@@ -0,0 +1,120 @@
+import React, {useState} from "react";
+import './style.css'
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import { Form, Col, InputGroup, Container, Row, Button, Accordion, Card } from "react-bootstrap";
+import MaskedInput from "react-maskedinput";
+
+function CadastroResponsavel () {
+    const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+      <>
+    <Header />
+    <Container>
+        <h1 className="titulo">Cadastro Responsável</h1>
+        <Row>
+        <Col xs={9}>
+            <h2 className="subtitulo">Nesta etapa faremos um cadastro especial para conhecermos melhor você</h2>
+        </Col>
+        </Row>
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form.Row className="linhaForm1">
+            <Form.Group as={Col} md="4" controlId="validationCustom01">            
+            <Form.Control
+                id="campoNome" 
+                required
+                type="text"
+                placeholder="Nome Completo"           
+            />           
+            </Form.Group>
+            <Form.Group as={Col} md="4" controlId="validationCustom02">        
+            <Form.Control
+                id="campoCPF"
+                required
+                type="text"
+                placeholder="CPF"                
+            />    
+            <Form.Control.Feedback type="invalid">
+                Gentileza informar um CPF válido.
+            </Form.Control.Feedback>        
+            </Form.Group>            
+        </Form.Row>
+        <Form.Row>
+            <Form.Group as={Col} md="6" controlId="validationCustom03">            
+            <Form.Control 
+            id="campoTel"
+            type="tel" 
+            placeholder="Telefone"  
+            />
+            <Form.Control.Feedback type="invalid">
+                Gentileza informar um telefone válido.
+            </Form.Control.Feedback>
+            </Form.Group>           
+        </Form.Row>
+        <Row>
+        <Col xs={9}>
+            <h2 className="subtitulo">Para finalizar o seu cadastro insira:</h2>
+        </Col>
+        </Row>
+        <Form.Row className="linhaForm1">
+            <Form.Group as={Col} md="4" controlId="validationCustom01">            
+            <Form.Control                
+                id="campoMail"
+                required
+                type="mail"
+                placeholder="E-mail"           
+            />    
+            <Form.Control.Feedback type="invalid">
+                Gentileza informar um e-mail válido.
+            </Form.Control.Feedback>        
+            </Form.Group>
+            <Form.Group as={Col} md="4" controlId="validationCustom02">        
+            <Form.Control
+                id="campoSenha"
+                required
+                type="password"
+                placeholder="Senha"                
+            />    
+            <Form.Control.Feedback type="invalid">
+                Escolha uma senha com no mínimo 06 dígitos
+            </Form.Control.Feedback>        
+            </Form.Group>            
+            </Form.Row>
+            <Form.Row>
+                <Form.Group as={Col} md="4" controlId="validationCustom02">        
+                <Form.Control
+                    id="campoqtdFilhos"
+                    required
+                    type="number"
+                    placeholder="Quantos filhos possui?"                
+                />    
+                <Form.Control.Feedback type="invalid">
+                    Número inválido
+                </Form.Control.Feedback>        
+                </Form.Group>            
+            </Form.Row>        
+            <Button id="btnEnviar" type="submit">Continuar</Button>
+        </Form>
+        <Footer />
+    </Container>
+    </>
+  );
+}
+
+
+    
+export default CadastroResponsavel;        
+    
+
+
