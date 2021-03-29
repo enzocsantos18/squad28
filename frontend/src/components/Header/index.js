6392f2033cc480247e412af0250d3d48f95fd5f6
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2021-03-29 15:22:33 -0300

    Ajustes básicos e inserção de logo header

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import './style.css'
-
+import logo from '../../assets/logo.svg'
+import { Link } from "react-router-dom";
 function Header() {
   return (
     <>
-      <header class="menu">
-
+      <header className="menu">
+        <Link to="/"> 
+         <img src={logo} alt="Logo da empresa Lapiseira"></img>
+        </Link>
       </header>
     </>
   );
