c5918b767f248415145608e8beee652efcac941c
IvanSzoboszlayJunior341 <32872975+IvanSzoboszlayJunior341@users.noreply.github.com>
2021-03-29 21:57:13 -0300

    Preparação do ambeinte
    Arrumando ambiente para react

---
+++ backend/src/app/controllers/ParentController.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import * as Yup from 'yup';
+import Parent from '../models/Parent';
+
+class ParentController {
+  async store(req: Request, res: Response) {
+    const { name, email, password } = req.body;
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required('Name is required').min(2).max(100),
+      email: Yup.string().required('Email is required').email(),
+      password: Yup.string().required('Password is required').min(8).max(16),
+    });
+
+    try {
+      await schema.validate(req.body);
+    } catch (e) {
+      return res.json(e);
+    }
+
+    const ParentRepository = getRepository(Parent);
+
+    const parentExists = await ParentRepository.findOne({ email });
+
+    if (parentExists) {
+      return res.status(400).json({ error: 'Parent already exists' });
+    }
+    const parent = ParentRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    try {
+      await ParentRepository.save(parent);
+
+      return res.json({
+        id: parent.id,
+        email: parent.email,
+      });
+    } catch (e) {
+      return res
+        .status(400)
+        .json({ error: 'Parent could not be created, try again later' });
+    }
+  }
+}
+
+export default new ParentController();
---
+++ backend/src/app/models/Parent.ts
@@ -0,0 +1,36 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BeforeInsert,
+  BeforeUpdate,
+  OneToMany,
+} from 'typeorm';
+import bcrypt from 'bcrypt';
+import Student from './Student';
+
+@Entity('Parent')
+class Parent {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword(): void {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
+
+  @OneToMany(type => Student, student => student.parent)
+  students: Student[];
+}
+
+export default Parent;
