2acb6a7c298bf0b1ca946a16c88284c6c6e99862
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2021-03-30 17:03:56 -0300

    Troca de nome na área da loja

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './pages/Home'
 import Login from './pages/Login';
-import DashboardStore from './pages/DashboardStore';
 import DetalheLista from './pages/DetalheLista';
 import AreaResponsavel from './pages/AreaResponsavel';
 import DetalheListaResponsavel from './pages/DetalheListaResponsavel';
+import AreaLoja from './pages/AreaLoja';
 
 
 function Routes() {
@@ -15,7 +15,7 @@ function Routes() {
         <Route path="/" exact component={Home} />
         <Route path="/lista/:id" component={DetalheLista} />
 
-        <Route path="/dashboard/store" component={DashboardStore}/>
+        <Route path="/areaLoja" component={AreaLoja}/>
         <Route path="/areaResponsavel" exact component={AreaResponsavel}/>
         <Route path="/areaResponsavel/lista/:id" component={DetalheListaResponsavel}/>
         <Route path="/login" component={Login}/>
