195c0a3c5f2802e8e54c3284d7f86d068516b382
IvanSzoboszlayJunior341 <32872975+IvanSzoboszlayJunior341@users.noreply.github.com>
2021-03-29 22:14:45 -0300

    Estrutura
    Estrutura dos componentes (form,img e texto)

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
-
+import { Container, Form, Col, Modal, Button} from "react-bootstrap";
+import imagemlogin from './fotologin.png'
 // import { Container } from './styles';
 
 function Login() {
   return (
-    <div>
-      <h1>Essa é o Login</h1>
-    </div>
+    <Container>
+      <h2>Faça o logim e vamos mudar o mundo!</h2>
+      <img src={imagemlogin }/>
+
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Control type="email" placeholder="Email" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Control type="password" placeholder="Senha" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+
+    </Container>
+
+
   );
 }
 
