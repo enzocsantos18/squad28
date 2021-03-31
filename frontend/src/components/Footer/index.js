4d606d6a114d5998a8f903598e671864af2a2702
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2021-03-30 21:54:31 -0300

    Ajustes no component Footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -5,7 +5,6 @@ function Footer() {
   return (
     <footer className="footer">
       <div>
-        <h4>Fale conosco:</h4>
         <p>Email: lapiseira@gmail.com</p>
         <p>Telefone: +55 (13) 99999-9999</p>
       </div>
