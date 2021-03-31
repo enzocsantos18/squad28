c0cdf45cbfc09d78e4f8f7d9c67d610f712b474d
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2021-03-31 10:41:10 -0300

    Remoção do backend da branch

--- backend/src/database/connect.ts
+++
@@ -1,8 +0,0 @@
-import { createConnection } from 'typeorm';
-
-createConnection()
-  .then(() => console.log('Connected to database'))
-  .catch(e => {
-    console.error(e);
-    console.error('Erro ao conectar ao banco');
-  });
