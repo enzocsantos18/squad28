35db62912689ca91e0a1ff4b6935278488e373ea
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2021-03-31 14:21:47 -0300

    Melhorias no cabeçalho

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -2,13 +2,17 @@ import React from "react";
 import './style.css'
 import logo from '../../assets/logo.svg'
 import { Link } from "react-router-dom";
-function Header() {
+
+function Header({children, linkLogo = "/"}) {
   return (
     <>
       <header className="menu">
-        <Link to="/"> 
+        <Link to={linkLogo}> 
          <img src={logo} alt="Logo da empresa Lapiseira"></img>
         </Link>
+        {
+          children
+        }
       </header>
     </>
   );
