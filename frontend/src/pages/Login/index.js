c136f67555d417a28aecf39d4b296fb5d55559cd
IvanSzoboszlayJunior341 <32872975+IvanSzoboszlayJunior341@users.noreply.github.com>
2021-03-31 21:45:05 -0300

    Responsividade
    Rolagem dos elemento

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -8,9 +8,6 @@ import api from '../../services/api';
 import { useHistory } from 'react-router';
 import Auth from '../../services/auth'
 
-
-
-
 function Login() {
 
   const history = useHistory();
@@ -18,12 +15,8 @@ function Login() {
     email: '',
     password: '',
   })
-
   const [isResponsavel, setIsResponsavel] = useState(true);
   const [erro, setErro] = useState(false);
-
-
-
 
 const handleInputChange = (event) => {
     setDados({
@@ -78,51 +71,41 @@ event.preventDefault();
     <>
     <Header/>
     <Container >
-      <Row>
-        <Col sm={6}>
-          <div className='grid_texto'>
-            <h1>Olá, somos a<br/> Lapiseira</h1>
-            
-            <p>Conectamos alunos com necessidades de materiais escolares com pessoas com vontade de mudar essa realidade.</p>
-          </div>
-        </Col>
-        
-        <Col sm={6}>
-          <div className='grid' >
-            <Row>
-              <Button onClick={() => setIsResponsavel(true)}>Responsavel</Button><Button onClick={() => setIsResponsavel(false)}>Papelaria</Button>
-              <div>
-                <h2 className='titulo_login'>Faça o login <br/>e vamos <br/>mudar o<br/> mundo!</h2>
-              </div>
-              <div>
-                <img className='img_login' alt="imagem ilustrativa login" src={imagemlogin}/>
-              </div>          
-            </Row>
-            
+      <div className='grid' >
+        <Row xs={2} md={8} lg={3}>
+
+          <Col>
+            <h2 className='titulo_login'>Faça o login <br/>e vamos <br/>mudar o<br/> mundo!</h2>
+          </Col>
+
+          <Col>
+            <img className='img_login' alt="imagem ilustrativa login" src={imagemlogin}/>
+          </Col> 
+
+          <Col xs={12} md={12} lg={4}>
             <Form id='form' onSubmit={handleSubmit}>
               <Form.Group>
-                <Form.Control id='input' type="email" placeholder="Email" onChange={handleInputChange}
-name="email" />
+                <Form.Control className='input' type="email" placeholder="Email" onChange={handleInputChange} name="email" />
               </Form.Group>
       
               <Form.Group>
-                <Form.Control id='inputsenha'                         onChange={handleInputChange}
-name="password" type="password" placeholder="Senha" />
+                <Form.Control className='input' onChange={handleInputChange} name="password" type="password" placeholder="Senha" />
               </Form.Group>
-             {erro && <span>Email e/ou senha inválidos.</span>}
-                <Button id="buttonEntar" type="submit">
-                  Login
-                </Button>
-            
-              
+              {erro && <span>Email e/ou senha inválidos.</span>}
+                <div className='buttonaliamento'>
+                  <Button id="buttonEntar" type="submit">
+                    Login
+                  </Button>
+                </div>
+                {/* <Button onClick={() => setIsResponsavel(true)}>Responsavel</Button>
+                <Button onClick={() => setIsResponsavel(false)}>Papelaria</Button> */}
             </Form>
+          </Col>
 
-          </div>
-        </Col>
-      </Row>
-
+        </Row>
+      </div>
     </Container>
-      <Footer/>
+    <Footer/>
     </>
   );
 }
