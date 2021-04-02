776b21004309163a924067e6753cc599067e5a03
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2021-04-01 21:51:20 -0300

    Validação form responsável

--- frontend/src/pages/CadastroResponsavel/index.js
+++ frontend/src/pages/CadastroResponsavel/index.js
@@ -1,197 +1,238 @@
-import React, {useState} from "react";
-import './style.css'
+import React, { useState } from "react";
+import "./style.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import { Form, Col,Container, Row, Button } from "react-bootstrap";
+import { Form, Col, Container, Row, Button } from "react-bootstrap";
 import MaskedInput from "react-maskedinput";
-import imgResp from "../../assets/responsavel.png"
+import imgResp from "../../assets/responsavel.png";
 import api from "../../services/api";
 import { useHistory } from "react-router-dom";
+import { Formik } from "formik";
+import * as Yup from 'yup';
 
-function CadastroResponsavel (props) {
-    const [validated, setValidated] = useState(false);
-    const history = useHistory();
+function CadastroResponsavel(props) {
+  const history = useHistory();
 
-    const [dados, setDados] = useState({
-        nome: '',
-        email: '',
-        cpf: '',
-        senha: '',
-    })
+  const validationSchema = Yup.object().shape({
+    nome: Yup.string().required('O campo nome é obrigatório').min(2, "O tamanho mínimo do campo é 2 caracteres").max(100),
+    email: Yup.string().required('O campo email é obrigatório').email('Preencha o campo email no formato correto'),
+    senha: Yup.string().required('O campo senha é obrigatório').min(8, "O campo senha deve ter no mínimo 8 caracteres").max(16, "A senha deve ter menos de 16 caracteres"),
+    telefone: Yup.string().required('O campo telefone é obrigatório').min(8).max(16),
+    cpf: Yup.string()
+      .required('O campo CPF é obrigatório')
+      .matches(
+        /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/,
+        'Preencha o campo cpf no formato correto',
+      ),
+  });
 
+  
 
+  async function handleFormik(values, metodos) {
+    metodos.setSubmitting(true);
 
-    const handleInputChange = (event) => {
-        setDados({
-            ...dados,
-            [event.target.name] : event.target.value
-        })
-    }
-
-  async function handleSubmit (event){
-    event.preventDefault();
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
 
     try{
-       const response = await api.post('/parent', {
-            name: dados.nome,
-            email: dados.email,
-            password: dados.senha
+
+        const {data} = await api.post('/parent', {
+            name: values.nome,
+            email: values.email,
+            cpf: values.cpf,
+            phone: values.telefone,
+            password: values.senha,
         })
 
-        if (response.status === 200){
-             history.push("/login");
-        }
-    }
-    catch(e){   
-        console.error(e)
+        history.push(`/login`)
+
+    }catch(e){
+        
     }
 
-    setValidated(true);
-  };
+    metodos.resetForm();
+    metodos.setSubmitting(false);
+  }
+
+
+ 
 
   return (
     <div className="main">
-    <Header />
-    <Container>   
-    <img id="imgRespon"src={imgResp} alt="Avatar Responsavel"/>
-        <Form className="formulario" noValidate validated={validated} onSubmit={handleSubmit}>
-            
-            <div className="formMenor">
-            <h1 className="titulo">Cadastro Responsável</h1>
-            <Row>
-                <Col xs={9}>
-                    <h2 className="subtitulo1">Nesta etapa faremos um cadastro inicial para conhecermos melhor você </h2>                      
-                </Col>
-            </Row>
-            <Form.Row className="linhaForm1">
-                <Form.Group className="campo" as={Col} md="4" controlId="validationCustom01">    
+      <Header />
+      <Container>
+        <img id="imgRespon" src={imgResp} alt="Avatar Responsavel" />
+        <Formik
+          initialValues={{
+            nome: "",
+            cpf: "",
+            telefone: "",
+            email: "",
+            senha: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(values, { setSubmitting, resetForm }) =>
+            handleFormik(values, { setSubmitting, resetForm })
+          }
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <Form className="formulario" onSubmit={handleSubmit}>
+              <div className="formMenor">
+                <h1 className="titulo">Cadastro Responsável</h1>
+                <Row>
+                  <Col xs={9}>
+                    <h2 className="subtitulo1">
+                      Nesta etapa faremos um cadastro inicial para conhecermos
+                      melhor você{" "}
+                    </h2>
+                  </Col>
+                </Row>
+                <Form.Row className="linhaForm1">
+                  <Form.Group
+                    className="campo"
+                    as={Col}
+                    md="4"
+                  >
                     <Form.Control
-                        onChange={handleInputChange}
-                        id="campoNome"
-                        name="nome" 
-                        required
-                        type="text"
-                        placeholder="Nome Completo"           
-                    />           
-                </Form.Group>
-            </Form.Row>  
-            <Form.Row> 
-                <Form.Group as={Col} md="4" controlId="validationCustom02">     
+                      id="campoNome"
+                      name="nome"
+                      required
+                      type="text"
+                      placeholder="Nome Completo"
+                      onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.nome}
+                    className={touched.nome && errors.nome ? "erro-campo" : null}
+                    />
+                    {touched.nome && errors.nome ? (
+                    <div className="erro">{errors.nome}</div>
+                    ): null}
+                  </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                  <Form.Group as={Col} md="4">
                     <MaskedInput
-                        onChange={handleInputChange}
-                        required
-                        type="text"
-                        name="cpf" 
-                        mask="111.111.111-11"
-                        placeholder="  CPF" 
-                        id="campoCPF"
-                        {...props}
-                        formatCharacters={{
-                        'W': {
-                            validate(char) { return /\w/.test(char ) },
-                            transform(char) { return char.toUpperCase() }
-                        }
-                        }}
-                    />   
+                      required
+                      type="text"
+                      name="cpf"
+                      mask="111.111.111-11"
+                      placeholder="  CPF"
+                      id="campoCPF"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.cpf}
+                      className={touched.cpf && errors.cpf ? "erro-campo" : null}
+                      {...props}
+                      formatCharacters={{
+                        W: {
+                          validate(char) {
+                            return /\w/.test(char);
+                          },
+                          transform(char) {
+                            return char.toUpperCase();
+                          },
+                        },
+                      }}
+                    />
+
+                    {touched.cpf && errors.cpf ? (
+                    <div className="erro">{errors.cpf}</div>
+                    ): null}
+                  </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                  <Form.Group as={Col} md="6">
+                    <MaskedInput
+                      type="text"
+                      mask="(11) 11111-1111"
+                      placeholder="  Telefone"
+                      name="telefone"
+                      id="campoTel"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.telefone}
+                      className={touched.telefone && errors.telefone ? "erro-campo" : null}
+                      {...props}
+                      formatCharacters={{
+                        W: {
+                          validate(char) {
+                            return /\w/.test(char);
+                          },
+                          transform(char) {
+                            return char.toUpperCase();
+                          },
+                        },
+                      }}
+                    />
+
                     
-                    {/*<Form.Control
-                        id="campoCPF"
-                        required
-                        type="text"
-                        placeholder="CPF"       
-                                 
-                    />*/}    
-                    <Form.Control.Feedback type="invalid">
-                        Gentileza informar um CPF válido.
-                    </Form.Control.Feedback>        
-                </Form.Group>            
-            </Form.Row>
-            <Form.Row>
-                <Form.Group as={Col} md="6" controlId="validationCustom03">         
-                    <MaskedInput
-                        onChange={handleInputChange}
-                        type="text"
-                        mask="(11) 11111-1111"
-                        placeholder="  Telefone" 
-                        name="telefone" 
+                    {touched.telefone && errors.telefone ? (
+                    <div className="erro">{errors.telefone}</div>
+                    ): null}
+                  </Form.Group>
+                </Form.Row>
+                <Row>
+                  <Col xs={10}>
+                    <h2 className="subtitulo2">
+                      Para finalizar o seu cadastro insira:
+                    </h2>
+                  </Col>
+                </Row>
+                <Form.Row className="linhaForm1">
+                  <Form.Group as={Col} md="4">
+                    <Form.Control
+                      id="campoMail"
+                      required
+                      name="email"
+                      type="mail"
+                      placeholder="E-mail"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}
+                      className={touched.email && errors.email ? "erro-campo" : null}
+                    />
+                           
+                    {touched.email && errors.email ? (
+                    <div className="erro">{errors.email}</div>
+                    ): null}
+                  </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                  <Form.Group as={Col} md="4">
+                    <Form.Control
+                      id="campoSenha"
+                      required
+                      name="senha"
+                      type="password"
+                      placeholder="Senha"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.senha}
+                      className={touched.senha && errors.senha ? "erro-campo" : null}
+                    />
+                  {touched.senha && errors.senha ? (
+                    <div className="erro">{errors.senha}</div>
+                    ): null}
+                  </Form.Group>
+                </Form.Row>
 
-                        id="campoTel"
-                        {...props}
-                        formatCharacters={{
-                        'W': {
-                            validate(char) { return /\w/.test(char ) },
-                            transform(char) { return char.toUpperCase() }
-                        }
-                        }}
-                    />   
-
-                    {/*<Form.Control 
-                    id="campoTel"
-                    type="tel" 
-                    placeholder="Telefone"  
-                    />*/}
-                    <Form.Control.Feedback type="invalid">
-                        Gentileza informar um telefone válido.
-                    </Form.Control.Feedback>
-                </Form.Group>           
-            </Form.Row>
-            <Row>
-                <Col xs={10}>
-                    <h2 className="subtitulo2">Para finalizar o seu cadastro insira:</h2>
-                </Col>
-            </Row>
-            <Form.Row className="linhaForm1">
-                <Form.Group as={Col} md="4" controlId="validationCustom01">            
-                    <Form.Control     
-                        onChange={handleInputChange}           
-                        id="campoMail"
-                        required
-                        name="email" 
-                        type="mail"
-                        placeholder="E-mail"           
-                    />    
-                    <Form.Control.Feedback type="invalid">
-                        Gentileza informar um e-mail válido.
-                    </Form.Control.Feedback>        
-                </Form.Group>
-            </Form.Row>     
-            <Form.Row> 
-                <Form.Group as={Col} md="4" controlId="validationCustom02">      
-                   <Form.Control
-                        onChange={handleInputChange}
-                        id="campoSenha"
-                        required
-                        name="senha" 
-                        type="password"
-                        placeholder="Senha"                
-                    />    
-                    <Form.Control.Feedback type="invalid">
-                        Escolha uma senha com no mínimo 08 dígitos
-                    </Form.Control.Feedback>        
-                </Form.Group>            
-            </Form.Row>
-            
-            <Button id="btnEnviar" type="submit">Continuar</Button>     
-              
-            </div>
-        </Form>        
-                  
-    </Container>
-    <Footer /> 
+                <Button id="btnEnviar" type="submit">
+                  Continuar
+                </Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </Container>
+      <Footer />
     </div>
   );
-
-  
 }
 
-
-    
-export default CadastroResponsavel;        
-
-
-
+export default CadastroResponsavel;
