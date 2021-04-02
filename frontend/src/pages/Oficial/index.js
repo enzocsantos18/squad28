7c191bbac72c4024244f547c768b78bbf56a6959
IvanSzoboszlayJunior341 <32872975+IvanSzoboszlayJunior341@users.noreply.github.com>
2021-04-01 22:47:46 -0300

    mobile
    Tela home construída.

--- frontend/src/pages/Oficial/index.js
+++ frontend/src/pages/Oficial/index.js
@@ -3,6 +3,7 @@ import './style.css';
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Login from "../Login"
+import CadastroResponsavel from "../CadastroResponsavel"
 import { Container,  Col, Button,Row,Card,CardGroup} from "react-bootstrap";
 import img_nosso from '../../assets/Rectangle2_home1.png' ;
 import elli from './Ellipse1.png';
@@ -13,7 +14,7 @@ import elli from './Ellipse1.png';
 function Oficial() {
     return (
         <>
-        {/* <Header/> */}
+        <Header/>
         <Container fluid>   
             <Row>        
                 <div id='fundo_inicio'>
@@ -22,101 +23,70 @@ function Oficial() {
                         <h1 className='h1_titulo '>Olá, somos a Lapiseira!</h1>
                     </div>
 
-                    <p className='p_conteudo'>Conectamos alunos com necessidades de materiais escolares a pessoas com vontade de mudar essa realidade.</p>
+                    <p>Conectamos alunos com<br/> 
+                                      necessidades de materiais<br/> 
+                                        escolares a pessoas com<br/>
+                                         vontade de mudar essa<br/>
+                                          realidade.</p>
                     
-                    <a href='#'><Button className='button' type='submint' >Login</Button></a>
-                </div>
-
+                    <a href='/Login'><Button className='button' type='submint' >Login</Button></a>
+                </div>              
+         
                 <h2>Nosso Objetivo</h2>
 
-                <Row xs={2} md={2} lg={2}>
-
-                    <Col><p>Acreditamos na educação acima de tudo e queremos que crianças não percam oportunidade de aprender mais por falta de material.</p>
+                <Row>
+                    <Col>
+                        <p id='gird_objetivo' className='texto_imagem'>Acreditamos na educação acima de tudo e queremos que crianças não percam oportunidade de aprender mais por falta de material.</p>
+                    </Col> 
+                    <Col >
+                        <div>
+                            <img src={img_nosso} id="img_nosso_objetivo"/>
+                        </div>
                     </Col>
-
-                    <Col><img src={img_nosso}></img></Col>
-
                 </Row>
-                <h2>Faça Parte</h2>
-                <Row xs={1} md={2} lg={2}>
-                    <Col>
-                        <p>Se você for o responsável por um aluno da rede pública que precisa de material escolar, cadastre-se abaixo</p>
-                        <a href='#'> <Button className='button' type='submint'>Responsável</Button></a>
+            
+                <h2 id='espacamento_h2'>Faça Parte</h2>
+
+                <Row xs={1} md={2} lg={2} className='grid'>
+                   
+                    <Col>             
+                        <p className='centralizarTexto'>Se você for o responsável por um aluno da rede pública que precisa de material escolar, cadastre-se abaixo.</p>
+                        <a href='/CadastroResponsavel' > <Button className='button2' type='submint'>Responsável</Button></a>
                     </Col>
                     <Col>
-                        <p>Se você for um(a) doador(a) e quiser acessar as listas, clique abaixo</p>
-                        <a href='#'> <Button className='button' type='submint'>Doador</Button></a>
+                        <p className='centralizarTexto'>Se você for um(a) doador(a) e quiser acessar as listas, clique abaixo.</p>
+                        <a href='#' > <Button className='button2' type='submint'>Doador</Button></a>
                     </Col>
+                   
                 </Row>
                 <h2>Depoimentos</h2>
 
-                {/* <Row xs={3} md={3} lg={3}>
-           
-                    <Col>
-                        <Card>
-                            <Card.Img variant="top" src={elli}className='img_card'/>
-                            <Card.Body>
-                            
-                                <Card.Text>
-                                    This is a wider card with supporting text below as a natural
-                                </Card.Text>
-                            </Card.Body>
-
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card>
-                            <Card.Img variant="top" src={elli} className='img_card'/>
-                            <Card.Body>
+                <Row xs={3} md={3} lg={3} id="test">
                         
-                            <Card.Text>
-                                This card has supporting text below as a natural lead-in to additional
-                                content.{' '}
-                            </Card.Text>
-                            </Card.Body>
-
-                        </Card>
+                    <Col className='col_card'>
+                        <div className='fundo_card'>
+                            <img src={elli} className='img_card'/>
+                            <p className='p_card'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
                     </Col>
-                    <Col>
-                        <Card>
-                            <Card.Img variant="top" src={elli} className='img_card' />
-                            <Card.Body>
-
-                            <Card.Text>
-                                This is a wider card with supporting text below as a natural 
-                            </Card.Text>
-                            </Card.Body>
-
-                        </Card>
-                    </Col> 
-           
-                </Row>*/}
-
-                <Row xs={3} md={3} lg={3}>
-                        
-                    <Col>
-                        <img src={elli} className='img_card'/>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <Col className='col_card'>
+                        <div className='fundo_card'>
+                            <img src={elli} className='img_card'/>
+                            <p className='p_card'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div> 
                     </Col>
-                    <Col>
-                        <img src={elli} className='img_card'/>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Col>
-                    <Col>
-                        <img src={elli} className='img_card'/>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <Col className='col_card'>
+                        <div className='fundo_card'>
+                            <img src={elli} className='img_card'/>
+                            <p className='p_card'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
                     </Col> 
            
                 </Row>
-
-
-
-
-
             </Row>
 
         </Container>
-        {/* <Footer/> */}
+        <Footer/>
         </>
     );
 }
