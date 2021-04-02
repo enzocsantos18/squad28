04294d8d384094e27599ac10af3f9c15a0a950e0
IvanSzoboszlayJunior341 <32872975+IvanSzoboszlayJunior341@users.noreply.github.com>
2021-04-02 01:23:31 -0300

    Melhoria
    Melhoria de rolagem da pagina Home(Official)

--- frontend/src/pages/Oficial/index.js
+++ frontend/src/pages/Oficial/index.js
@@ -16,25 +16,26 @@ function Oficial() {
         <>
         <Header/>
         <Container fluid>   
-            <Row>        
+            <Row>    
                 <div id='fundo_inicio'>
-                   
+                
                     <div id='posicao_h1_titulo'>
                         <h1 className='h1_titulo '>Olá, somos a Lapiseira!</h1>
                     </div>
 
                     <p>Conectamos alunos com<br/> 
-                                      necessidades de materiais<br/> 
+                                    necessidades de materiais<br/> 
                                         escolares a pessoas com<br/>
-                                         vontade de mudar essa<br/>
-                                          realidade.</p>
+                                        vontade de mudar essa<br/>
+                                        realidade.</p>
                     
                     <a href='/Login'><Button className='button' type='submint' >Login</Button></a>
-                </div>              
-         
+                </div>   
+             
+
                 <h2>Nosso Objetivo</h2>
 
-                <Row>
+                {/* <Row xs={2} md={2} >
                     <Col>
                         <p id='gird_objetivo' className='texto_imagem'>Acreditamos na educação acima de tudo e queremos que crianças não percam oportunidade de aprender mais por falta de material.</p>
                     </Col> 
@@ -43,22 +44,25 @@ function Oficial() {
                             <img src={img_nosso} id="img_nosso_objetivo"/>
                         </div>
                     </Col>
-                </Row>
-            
-                <h2 id='espacamento_h2'>Faça Parte</h2>
+                </Row> */}
 
-                <Row xs={1} md={2} lg={2} className='grid'>
+                <h2 id='espacamento_h2'>Faça Parte</h2>
+                    
+           
+                <Row xs={1} md={2} className='grid'>
                    
                     <Col>             
                         <p className='centralizarTexto'>Se você for o responsável por um aluno da rede pública que precisa de material escolar, cadastre-se abaixo.</p>
                         <a href='/CadastroResponsavel' > <Button className='button2' type='submint'>Responsável</Button></a>
                     </Col>
+
                     <Col>
                         <p className='centralizarTexto'>Se você for um(a) doador(a) e quiser acessar as listas, clique abaixo.</p>
                         <a href='#' > <Button className='button2' type='submint'>Doador</Button></a>
                     </Col>
                    
                 </Row>
+
                 <h2>Depoimentos</h2>
 
                 <Row xs={3} md={3} lg={3} id="test">
