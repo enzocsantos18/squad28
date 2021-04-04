6a95cb07b021da1477b1d228c7e792b2256f196a
IvanSzoboszlayJunior341 <32872975+IvanSzoboszlayJunior341@users.noreply.github.com>
2021-04-04 09:52:39 -0300

    Delete
    Delete da main

--- frontend/src/helpers/tratamentoDinheiro.js
+++
@@ -1,6 +0,0 @@
-export default function numeroToReal(valor) {
-  var numero = valor.toFixed(2).split('.');
-  numero[0] = "R$ " + numero[0].split(/(?=(?:...)*$)/).join('.');
-  return numero.join(',');
-}
-
